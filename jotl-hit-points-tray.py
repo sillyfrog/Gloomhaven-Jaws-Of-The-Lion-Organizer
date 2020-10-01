449fbab6958b90cbba1dbf5f4ed3cecfd503b33f
Sillyfrog <sillyfrog@users.noreply.github.com>
2020-10-01 23:10:08 +1000

    Hit point tokens trays

--- jotl-hit-points-tray.py
+++ jotl-hit-points-tray.py
@@ -11,13 +11,13 @@ FLOOR = 1
 WALL = 1
 TEXT_D = 0.4
 
-HP_1_D = 14
-HP_3_D = 18
-HP_10_D = 23
+HP_1_D = 14.1
+HP_3_D = 18.2
+HP_10_D = 23.3
 
-TRAY_W = 57.5
+TRAY_W = 58
 TRAY_D = 42
-TRAY_H = 19
+TRAY_H = 20
 
 
 def roundedCube(size, radius, sidesonly):
@@ -49,7 +49,7 @@ def hitpoint_tray_1():
     loc1 = [HP_1_D / 2 + WALL, TRAY_W - (HP_1_D / 2)]
     loc2 = [TRAY_D - (HP_1_D / 2 + WALL), TRAY_W - (HP_1_D / 2)]
     loc3 = [loc2[0] + WALL, loc2[1] - (HP_1_D + WALL)]
-    loc4 = [(loc1[0] + loc3[0]) / 2, (loc1[1] + loc3[1]) / 2]
+    loc4 = [(loc1[0] + loc3[0]) / 2 - 0.1, (loc1[1] + loc3[1]) / 2 - 0.1]
     o = translate(loc1)(rotate([0, 0, -90])(stack))
     o += translate([loc2[0] + WALL / 2, loc2[1]])(rotate([0, 0, 180])(stack))
     o += translate(loc3)(rotate([0, 0, 180])(stack))
@@ -64,7 +64,7 @@ def hitpoint_tray_2():
     o += translate([HP_3_D / 2, HP_3_D / 2 + WALL + HP_3_D + WALL])(stack3)
 
     stack10 = makestack(HP_10_D)
-    o += translate([TRAY_D - (HP_10_D / 2), HP_10_D / 2 + WALL])(
+    o += translate([TRAY_D - (HP_10_D / 2), HP_10_D / 2 + WALL + 2.09])(
         rotate([0, 0, 180])(stack10)
     )
 
